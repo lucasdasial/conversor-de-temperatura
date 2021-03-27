2efb02151603de455f2b2f0e19255eb2fbcaf6d3
Lucas Alves <as.lucasalves@gmail.com>
2021-03-27 16:49:24 -0300

    consertei a ordem do escopo

--- script.js
+++ script.js
@@ -16,46 +16,44 @@ let btnConverter = document.querySelector("input#btn-converter")
 let = printResultado = document.querySelector("h2#resultado")
 let temperatura = 0
 
-
+function calcular(){
+    // de celsius para as demais
+    if (mediTemp1 == 1 && mediTemp2 == 4){
+        temperatura = valorInserido 
+    }
+    else if(mediTemp1 == 1 && meditemp2 == 5){
+        temperatura = valorInserido * 9/5 +32
+    }
+    else if (mediTemp1 == 1 && mediTemp2 == 6){
+        temperatura = valorInserido + 273
+    }
+    // de fahren para as outras
+    else if (medTemp1 == 2 && mediTemp2 == 4){
+        temperatura = (valorInserido - 32) * 5/9
+    }
+    else if (medTemp1 == 2 && mediTemp2 == 5){
+        temperatura = valorInserido
+    }
+    else if (medTemp1 == 2 && mediTemp2 == 6){
+        temperatura = (valorInserido - 32) * 5/9 +273
+    }
+    //de kelin para as demais
+    else if (medTemp1 == 3 && mediTemp2 == 4){
+        temperatura = valorInserido - 273
+    }
+    else if (medTemp1 == 3 && mediTemp2 == 5){
+        temperatura = (valorInserido - 273) * 9/5 +32
+    }
+    else if(medTemp1 == 3 && mediTemp2 == 6){
+        temperatura = valorInserido
+    }else {
+        printResultado.innerHTML = ("Prfv preencha os campos a cima")
+    }
+    return temperatura;
+}
 
 btnConverter.addEventListener("click", ()=>{
-
-    ()=>{
-        // de celsius para as demais
-        if (mediTemp1 == 1 && mediTemp2 == 4){
-            temperatura = valorInserido 
-        }
-        else if(mediTemp1 == 1 && meditemp2 == 5){
-            temperatura = valorInserido * 9/5 +32
-        }
-        else if (mediTemp1 == 1 && mediTemp2 == 6){
-            temperatura = valorInserido + 273
-        }
-        // de fahren para as outras
-        else if (medTemp1 == 2 && mediTemp2 == 4){
-            temperatura = (valorInserido - 32) * 5/9
-        }
-        else if (medTemp1 == 2 && mediTemp2 == 5){
-            temperatura = valorInserido
-        }
-        else if (medTemp1 == 2 && mediTemp2 == 6){
-            temperatura = (valorInserido - 32) * 5/9 +273
-        }
-        //de kelin para as demais
-        else if (medTemp1 == 3 && mediTemp2 == 4){
-            temperatura = valorInserido - 273
-        }
-        else if (medTemp1 == 3 && mediTemp2 == 5){
-            temperatura = (valorInserido - 273) * 9/5 +32
-        }
-        else if(medTemp1 == 3 && mediTemp2 == 6){
-            temperatura = valorInserido
-        }else {
-            printResultado.innerHTML = ("Porfavor preencha os campos a cima")
-        }
-        return temperatura;
-    }
-    
+    calcular()
     printResultado.innerHTML = temperatura
 })
 
