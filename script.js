84b80ad428a38c9226a0c5110b4ce49c177f60c2
Lucas Alves <as.lucasalves@gmail.com>
2021-03-27 19:09:20 -0300

    refatorei mas falta arrumar o change

--- script.js
+++ script.js
@@ -1,61 +1,47 @@
-function de(valorDe){
-    var mediTemp1 = valorDe;
-    console.log( mediTemp1);
-    return mediTemp1;
+let numInserido = document.getElementById("entrada");
+let resultado = document.getElementById("resultado");
+let medPrimaria = document.getElementById("medidaPrimaria");
+let medSecundaria = document.getElementById("medidaSecundaria");
+let medPriValor, medidaSecValor;
+
+/*numInserido.addEventListener("keyup",()=>{
+    resultado.value = numInserido.value
+})*/
+numInserido.addEventListener("keyup", temperatura);
+medPrimaria.addEventListener("change", temperatura);
+medSecundaria.addEventListener("change", temperatura);
+
+medPriValor = medPrimaria.value
+medidaSecValor = medSecundaria.value
+
+
+function temperatura(){
+    //alert("trocou")
+
+    //de celsius para:
+    if(medPriValor == "celsius" && medidaSecValor == "celsius"){
+        resultado.value = numInserido.value
+    }else if(medPriValor == "celsius" && medidaSecValor == "fahren"){
+        resultado.value = numInserido.value * 9/5 + 32
+    }else if(medPriValor == "celsius" && medidaSecValor == "kelvin"){
+        resultado.value = numInserido.value + 273
+    }
+
+    //de fahren pra :
+    if(medPriValor == "fahren" && medidaSecValor == "celsius"){
+        resultado.value = (numInserido.value - 32) * 5/9
+    }else if(medPriValor == "fahren" && medidaSecValor == "fahren"){
+        resultado.value = numInserido.value 
+    }else if(medPriValor == "fahren" && medidaSecValor == "kelvin"){
+        resultado.value = (numInserido.value - 32) * 5/9 + 273
+    }
+
+    //de kelvin para:
+    if(medPriValor == "kelvin" && medidaSecValor == "celsius"){
+        resultado.value = numInserido.value - 273
+    }else if(medPriValor == "kelvin" && medidaSecValor == "fahren"){
+        resultado.value = (numInserido.value -273) * 9/5 +32
+    }else if(medPriValor == "kelvin" && medidaSecValor == "kelvin"){
+        resultado.value = numInserido.value 
+    }
 }
-
-function para(valorPara){
-    var mediTemp2 = valorPara;
-    console.log(mediTemp2);
-    return mediTemp2;
-}
-// o evento das funçoes a cima foram declaradas no index.html (onchage) nas tags selections
-
-let valorInserido = document.querySelector("input#value")
-let btnConverter = document.querySelector("input#btn-converter") 
-let = printResultado = document.querySelector("h2#resultado")
-let temperatura = 0
-
-function calcular(){
-    // de celsius para as demais
-    if (mediTemp1 == 1 && mediTemp2 == 4){
-        temperatura = valorInserido 
-    }
-    else if(mediTemp1 == 1 && meditemp2 == 5){
-        temperatura = valorInserido * 9/5 +32
-    }
-    else if (mediTemp1 == 1 && mediTemp2 == 6){
-        temperatura = valorInserido + 273
-    }
-    // de fahren para as outras
-    else if (medTemp1 == 2 && mediTemp2 == 4){
-        temperatura = (valorInserido - 32) * 5/9
-    }
-    else if (medTemp1 == 2 && mediTemp2 == 5){
-        temperatura = valorInserido
-    }
-    else if (medTemp1 == 2 && mediTemp2 == 6){
-        temperatura = (valorInserido - 32) * 5/9 +273
-    }
-    //de kelin para as demais
-    else if (medTemp1 == 3 && mediTemp2 == 4){
-        temperatura = valorInserido - 273
-    }
-    else if (medTemp1 == 3 && mediTemp2 == 5){
-        temperatura = (valorInserido - 273) * 9/5 +32
-    }
-    else if(medTemp1 == 3 && mediTemp2 == 6){
-        temperatura = valorInserido
-    }else {
-        printResultado.innerHTML = ("Prfv preencha os campos a cima")
-    }
-    return temperatura;
-}
-
-btnConverter.addEventListener("click", ()=>{
-    calcular()
-    printResultado.innerHTML = temperatura
-})
-
-
-   
